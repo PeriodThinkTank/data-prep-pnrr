c3e90736d4f145a5ebab5437f7501eaf090c7404
Giacomo <giacomo.saroldi@gmail.com>
2024-06-20 20:55:15 +0200

    Update links in the sources file

--- src/sources.py
+++ src/sources.py
@@ -4,9 +4,14 @@ P01_FILENAME = "cup_csv.csv"
 P02_URL = "https://www.opencup.gov.it/portale/documents/21195/299152/OpendataLocalizzazione.zip"
 P02_FILENAME = "OpenCup_Localizzazione.csv"
 
+P03_FILES = "https://dati.anticorruzione.it/opendata/dataset/misurepremiali-pnrrpnc"
+P03_URL = "https://dati.anticorruzione.it/opendata/download/dataset/misurepremiali-pnrrpnc/filesystem/misurepremiali-pnrrpnc_csv.zip"
+P03_FILENAME = "misurepremiali-pnrrpnc_csv.csv"
 
-P03_URL = "https://dati.anticorruzione.it/opendata/dataset/misurepremiali-pnrrpnc"
-P04_URL = "https://dati.anticorruzione.it/opendata/dataset/indicatori-pnrrpnc"
+P04_FILES = "https://dati.anticorruzione.it/opendata/dataset/indicatori-pnrrpnc"
+P04_URL = "https://dati.anticorruzione.it/opendata/download/dataset/indicatori-pnrrpnc/filesystem/indicatori-pnrrpnc_csv.zip"
+P04_FILENAME = "indicatori-pnrrpnc_csv.csv"
+
 P05_URLs = {
     2020: "https://dati.anticorruzione.it/opendata/dataset/cig-2020",
     2021: "https://dati.anticorruzione.it/opendata/dataset/cig-2021",
